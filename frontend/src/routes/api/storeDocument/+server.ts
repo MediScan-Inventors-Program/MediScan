d776bf31bd674c1104b13c1c59fffa185fb2453b
Brendan Early <mymindstorm@evermiss.net>
2023-10-27 14:33:23 -0500

    add a timestamp

--- frontend/src/routes/api/storeDocument/+server.ts
+++ frontend/src/routes/api/storeDocument/+server.ts
@@ -2,12 +2,14 @@ import { json } from '@sveltejs/kit'
 import { client } from '$lib/server/elastic'
 
 export async function POST({ _request, _cookies }) {
+    const now = new Date(Date.now());
     await client.index({
         index: 'mediscan_events',
-        id: Date.now().toString(),
+        id: crypto.randomUUID(),
         document: {
             test: true,
-            number: 50.123
+            number: 50.123,
+            timestamp: now.toISOString()
         }
     });
 
