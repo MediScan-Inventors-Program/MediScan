0ac6e8f495996423bc4848cba537e062bee6d63b
nathanlu <nathanlumax@gmail.com>
2023-11-13 14:39:42 -0600

    add daisy ui button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,13 +2,17 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div>
     <button on:click={storeDocument}>Send doc to elasticsearch</button>
+
+    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+    </button>
 </div>
 
 <script lang="ts">
     async function storeDocument() {
         await fetch('/api/storeDocument', {
             method: 'POST',
-            body: JSON.stringify({ "do": "thing" }),
+            body: JSON.stringify({"do": "thing"}),
             headers: {
                 'Content-Type': 'application/json'
             }
