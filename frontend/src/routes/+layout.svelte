0faad328443b03c7762ef3de7e774cf40376b02d
nathanlu <nathanlumax@gmail.com>
2023-11-13 14:57:52 -0600

    add meta tags and additional pages for structure

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
     import "../app.css";
 </script>
+
+<svelte:head>
+    <title>MediScan</title>
+    <meta name="description" content="MediScan - Building trust through medical transparency"/>
+    <meta name="author" content="MediScan"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta property="og:title" content="MediScan"/>
+    <meta property="og:description" content="MediScan - Building trust through medical transparency"/>
+    <meta property="og:image" content="https://ibb.co/qFZMgxT"/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Nate Lu" />
+</svelte:head>
 
 <slot />
