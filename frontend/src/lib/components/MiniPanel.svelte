764ff8f4ad5cf17aa565d28938ecb4a0d13a31c8
nathanlu <nathanlumax@gmail.com>
2023-11-14 18:02:06 -0600

    made minipanels

---
+++ frontend/src/lib/components/MiniPanel.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+
+    export let icon = 'fa fa-ellipsis-v';
+    export let panelName = '';
+    export let panelValue = '';
+    export let percentage = 0;
+</script>
+
+<div class="rounded-lg border-2 border-secondary p-5 bg-white flex-grow w-40">
+    <div class="rounded-xl bg-primary-100 flex items-center p-2 w-min">
+        <Icon icon="{icon}" class="h-min text-text w-6"/>
+    </div>
+    <h3 class="text-2xl font-bold mt-5 text-text">{panelValue}</h3>
+    <div class="flex justify-between align-middle">
+        <p class="text-secondary-500 whitespace-nowrap">{panelName}</p>
+        <p class="{percentage>0 ? 'text-success' : 'text-danger'} whitespace-nowrap">
+            {percentage > 0 ? '+' : ''}{percentage} {percentage > 0 ? '↑' : '↓'}
+        </p>
+    </div>
+</div>
--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,28 +1,60 @@
 <script lang="ts">
-    import Icon from "@iconify/svelte";
+    import MiniPanel from "$lib/components/MiniPanel.svelte";
 
-    async function storeDocument() {
-        await fetch('/api/storeDocument', {
-            method: 'POST',
-            body: JSON.stringify({"do": "things"}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
+    // async function storeDocument() {
+    //     await fetch('/api/storeDocument', {
+    //         method: 'POST',
+    //         body: JSON.stringify({"do": "things"}),
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    // }
 </script>
 
-<h1>Hello world!</h1>
-<h1 class="font-poppins">Hello world!</h1>
-<h1 class="font-inter">Hello worlds!</h1>
-<Icon icon="solar:moon-bold-duotone" />
 
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div>
-    <button on:click={storeDocument}>Send doc to elasticsearch</button>
+<!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
+<!--<div>-->
+<!--    <button on:click={storeDocument}>Send doc to elasticsearch</button>-->
 
-    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-    </button>
+<!--    <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">-->
+<!--        Button-->
+<!--    </button>-->
+<!--</div>-->
+
+<div class="pt-10">
+    <h1 class="text-3xl font-semibold">Dashboard</h1>
+    <div class="mt-5 flex justify-between align-middle gap-x-8 gap-y-5 flex-wrap">
+        <MiniPanel
+                icon="uil:heart-rate"
+                panelName="Medical Devices"
+                panelValue="17"
+                percentage={2}>
+        </MiniPanel>
+        <MiniPanel
+                icon="solar:shield-cross-outline"
+                panelName="CVEs"
+                panelValue="12"
+                percentage={-1}>
+        </MiniPanel>
+        <MiniPanel
+                icon="material-symbols:exclamation"
+                panelName="Recalls"
+                panelValue="5"
+                percentage={3}>
+        </MiniPanel>
+        <MiniPanel
+                icon="material-symbols:check"
+                panelName="Compliance"
+                panelValue="65"
+                percentage={6}>
+        </MiniPanel>
+        <MiniPanel
+                icon="fluent:question-24-filled"
+                panelName="Unknown Devices"
+                panelValue="8"
+                percentage={-8}>
+        </MiniPanel>
+    </div>
 </div>
